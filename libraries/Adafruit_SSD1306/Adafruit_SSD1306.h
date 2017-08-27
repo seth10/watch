e1080091f343cfc315211706fd8f6ae7ff6c7300
Seth T <seth10@users.noreply.github.com>
2017-08-26 21:25:49 -0400

    Minimize library and example
    My target is the Adafruit Trinket 5V, which only has 8k of flash and 1/2k of ram. I removed the Adafruit logo literal from the library which was used to initialize the buffer. Now it should start empty. I've also removed most of the example, keeping just what I may use.

--- libraries/Adafruit_SSD1306/Adafruit_SSD1306.h
+++ libraries/Adafruit_SSD1306/Adafruit_SSD1306.h
@@ -56,44 +56,10 @@ All text above, and the splash screen must be included in any redistribution
 // Address for 128x32 is 0x3C
 // Address for 128x64 is 0x3D (default) or 0x3C (if SA0 is grounded)
 
-/*=========================================================================
-    SSD1306 Displays
-    -----------------------------------------------------------------------
-    The driver is used in multiple displays (128x64, 128x32, etc.).
-    Select the appropriate display below to create an appropriately
-    sized framebuffer, etc.
+#define SSD1306_128_64
 
-    SSD1306_128_64  128x64 pixel display
-
-    SSD1306_128_32  128x32 pixel display
-
-    SSD1306_96_16
-
-    -----------------------------------------------------------------------*/
-//   #define SSD1306_128_64
-   #define SSD1306_128_32
-//   #define SSD1306_96_16
-/*=========================================================================*/
-
-#if defined SSD1306_128_64 && defined SSD1306_128_32
-  #error "Only one SSD1306 display can be specified at once in SSD1306.h"
-#endif
-#if !defined SSD1306_128_64 && !defined SSD1306_128_32 && !defined SSD1306_96_16
-  #error "At least one SSD1306 display must be specified in SSD1306.h"
-#endif
-
-#if defined SSD1306_128_64
-  #define SSD1306_LCDWIDTH                  128
-  #define SSD1306_LCDHEIGHT                 64
-#endif
-#if defined SSD1306_128_32
-  #define SSD1306_LCDWIDTH                  128
-  #define SSD1306_LCDHEIGHT                 32
-#endif
-#if defined SSD1306_96_16
-  #define SSD1306_LCDWIDTH                  96
-  #define SSD1306_LCDHEIGHT                 16
-#endif
+#define SSD1306_LCDWIDTH 128
+#define SSD1306_LCDHEIGHT 64
 
 #define SSD1306_SETCONTRAST 0x81
 #define SSD1306_DISPLAYALLON_RESUME 0xA4
