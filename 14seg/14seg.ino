4a05579c4bb9e97a3ea750a98f1e58285dbbabab
Seth T <seth10@users.noreply.github.com>
2017-08-30 00:07:05 -0400

    Brightness test with digit separator

--- 14seg/14seg.ino
+++ 14seg/14seg.ino
@@ -4,15 +4,16 @@ Adafruit_AlphaNum4 alpha4;
 
 void setup() {
   alpha4.begin();
-  alpha4.setBrightness(0);
-  alpha4.writeDigitAscii(1, 14);
+  alpha4.writeDigitAscii(0, 'B');
+  alpha4.writeDigitRaw(1, 0b0100000001010000);
+  //alpha4.writeDigitRaw(pgm_read_word(alphafonttable+'r') | 0b1<<14);
 }
 
 void loop() {
-  for (uint8_t i=0; i<16; i++) {
-    alpha4.writeDigitRaw(0, 1<<i);
-    alpha4.writeDigitAscii(2, '0'+i/10);
-    alpha4.writeDigitAscii(3, '0'+i%10);
+  for (uint8_t b=0; b<16; b++) {
+    alpha4.setBrightness(b);
+    alpha4.writeDigitAscii(2, '0'+b/10);
+    alpha4.writeDigitAscii(3, '0'+b%10);
     alpha4.writeDisplay();
     delay(500);
   }
