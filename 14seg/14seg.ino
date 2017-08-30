24deb91ad507d39936038a81883495403e228382
Seth T <seth10@users.noreply.github.com>
2017-08-29 23:58:38 -0400

    Brightness test

--- 14seg/14seg.ino
+++ 14seg/14seg.ino
@@ -1,31 +1,19 @@
 #include "Adafruit_LEDBackpack.h"
 
-char *message = "    Hello world!    ";
-
 Adafruit_AlphaNum4 alpha4;
 
 void setup() {
   alpha4.begin();
-
-  for (uint8_t i=0; i<strlen(message)-4; i++) {
-    alpha4.writeDigitAscii(0, message[i]);
-    alpha4.writeDigitAscii(1, message[i+1]);
-    alpha4.writeDigitAscii(2, message[i+2]);
-    alpha4.writeDigitAscii(3, message[i+3]);
-    alpha4.writeDisplay();
-    delay(200);
-  }
-
-  alpha4.clear();
-  alpha4.writeDisplay();
-  delay(1000);
+  alpha4.writeDigitAscii(0, 'B');
+  alpha4.writeDigitAscii(1, 'r');
 }
 
 void loop() {
-  for (uint8_t i=0; i<4; i++) {
-    alpha4.writeDigitRaw((i+4-1)%4, 0x0);
-    alpha4.writeDigitRaw(i, 0xFFFF);
+  for (uint8_t b=0; b<16; b++) {
+    alpha4.setBrightness(b);
+    alpha4.writeDigitAscii(2, '0'+b/10);
+    alpha4.writeDigitAscii(3, '0'+b%10);
     alpha4.writeDisplay();
-    delay(200);
+    delay(500);
   }
 }
