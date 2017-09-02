110e9e4208282beed863a44a1059af42684b08f4
Seth T <seth10@users.noreply.github.com>
2017-09-02 13:35:40 -0400

    Smallest timer example
    I wouldn't think enabling the Timer/Counter1 *overflow* interrupt would be necessary, but without it the ISR is not executed

--- trinket_timer/trinket_timer.ino
+++ trinket_timer/trinket_timer.ino
@@ -1,27 +1,16 @@
 volatile boolean state = false;
 
 ISR (TIMER1_OVF_vect) {
-  TIMSK &= ~_BV(TOIE1);
-  sei();
   state = !state;
   digitalWrite(LED_BUILTIN, state);
-  TIMSK |= _BV(TOIE1);
 }
 
 void setup() {
-  // Timer1 set to CK/1024  ~10 (8) hZ at 8 MHz clock rate,
-  // for other CPU speeds select correct divider bits per data sheet
-  // http://www.atmel.com/Images/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf#page=89
-  // 8hZ: 13, 11, 10
-  // 4hZ: 13, 12
-  // 2hZ: 13, 12, 10
-  // 1hZ: 13, 12, 11
-  //.5hZ: 13, 12, 11, 10
-  TCCR1 |= _BV(CS12) | _BV(CS13);// | _BV(CS11) | _BV(CS10);
-  //TCCR1 &= ~_BV(CS10);
-  TIMSK |= _BV(TOIE1);  // Enable Timer/Counter1 Overflow Interrupt
-
-  sei(); // enable interrupts
+  // set divider bits of Timer1 to PCK/4096, which is 2Hz at the 8MHz clock rate
+  // http://www.atmel.com/Images/Atmel-2586-AVR-8-bit-Microcontroller-ATtiny25-ATtiny45-ATtiny85_Datasheet.pdf#page=90
+  TCCR1 |= _BV(CS13) | _BV(CS12);
+  // enable Timer/Counter1 Overflow Interrupt
+  TIMSK |= _BV(TOIE1);
 }
 
 void loop() {
