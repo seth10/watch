b368f4283c08589b1633105001cfe32aa41eec6c
Seth T <seth10@users.noreply.github.com>
2017-09-01 21:06:08 -0400

    Integrate OLED in button example

--- button/button.ino
+++ button/button.ino
@@ -1,3 +1,7 @@
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display;
+
 volatile int n = 0;
 
 void isr() {
@@ -8,10 +12,15 @@ void setup() {
   Serial.begin(9600);
   pinMode(7, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(7), isr, FALLING);
+  display.begin();
+  display.setTextSize(8);
+  display.setTextColor(WHITE);
 }
 
 void loop() {
-  Serial.print('\r');
-  Serial.print(n);
-  delay(200);
+  display.clearDisplay();
+  display.setCursor((128-(8*5))/2, 0);
+  display.println(n);
+  display.display();
+  delay(50);
 }
