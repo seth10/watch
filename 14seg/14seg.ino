134e72faf02c5449367538b08abf38c940c9a191
Seth T <seth10@users.noreply.github.com>
2017-09-02 12:34:46 -0400

    Increment minutes
    For some reason every 60 seconds didn't work, so insetad keep track of half minutes

--- 14seg/14seg.ino
+++ 14seg/14seg.ino
@@ -3,17 +3,33 @@ Adafruit_AlphaNum4 alpha4;
 
 volatile byte minutes = 30, hours = 9;
 
+unsigned long nextHalfMinuteUpdate = millis()+30*1000;
+boolean halfMinute = false;
+
 void setup() {
   alpha4.begin();
   alpha4.setBrightness(0);
 }
 
 void loop() {
+  if (millis() > nextHalfMinuteUpdate) {
+    nextHalfMinuteUpdate += 30*1000;
+    halfMinute = !halfMinute;
+    if (!halfMinute) {
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+        if (hours == 24) hours = 0;
+      }
+    }
+  }
+
   if (hours < 10)
     alpha4.writeDigitAscii(0, ' ');
   else
-    alpha4.writeDigitAscii(0, '0'+(hours/10));
-  alpha4.writeDigitAscii(1, '0'+(hours%10));
+    alpha4.writeDigitAscii(0, '0'+(hours/10), hours>=12);
+  alpha4.writeDigitAscii(1, '0'+(hours%10), true);
   alpha4.writeDigitAscii(2, '0'+(minutes/10));
   alpha4.writeDigitAscii(3, '0'+(minutes%10));
   alpha4.writeDisplay();
