64752b6a87e6258b1a9592db4a304f99fe1777d3
Seth T <seth10@users.noreply.github.com>
2017-09-01 22:45:44 -0400

    Add barebones 14-segment watch sketch
    Button input omited for now

--- 14seg/14seg.ino
+++ 14seg/14seg.ino
@@ -1,20 +1,20 @@
 #include "Adafruit_LEDBackpack.h"
-
 Adafruit_AlphaNum4 alpha4;
+
+volatile byte minutes = 30, hours = 9;
 
 void setup() {
   alpha4.begin();
-  alpha4.writeDigitAscii(0, 'B');
-  alpha4.writeDigitRaw(1, 0b0100000001010000);
-  //alpha4.writeDigitRaw(pgm_read_word(alphafonttable+'r') | 0b1<<14);
+  alpha4.setBrightness(0);
 }
 
 void loop() {
-  for (uint8_t b=0; b<16; b++) {
-    alpha4.setBrightness(b);
-    alpha4.writeDigitAscii(2, '0'+b/10);
-    alpha4.writeDigitAscii(3, '0'+b%10);
-    alpha4.writeDisplay();
-    delay(500);
-  }
+  if (hours < 10)
+    alpha4.writeDigitAscii(0, ' ');
+  else
+    alpha4.writeDigitAscii(0, '0'+(hours/10));
+  alpha4.writeDigitAscii(1, '0'+(hours%10));
+  alpha4.writeDigitAscii(2, '0'+(minutes/10));
+  alpha4.writeDigitAscii(3, '0'+(minutes%10));
+  alpha4.writeDisplay();
 }
