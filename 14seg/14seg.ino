9b08e71b22bf46a91b036b36eef61a48f58254b1
Seth T <seth10@users.noreply.github.com>
2017-09-02 12:47:45 -0400

    Move blinking seconds dot to end
    Left is the longest units of time (hours), to the right of that is a more specific unit of time (minutes), so the most specific unit of time (seconds) should be to the *right* of /that/. Also leave the middle-most decimal to separate the hours from the minutes.

--- 14seg/14seg.ino
+++ 14seg/14seg.ino
@@ -4,7 +4,7 @@ Adafruit_AlphaNum4 alpha4;
 byte seconds = 0,
      minutes = 30,
      hours = 9;
-boolean showColonDelimiter = true;
+boolean blinkSeconds = true;
 unsigned long nextSecond = millis()+1000;
 
 void incrementSeconds() {
@@ -20,7 +20,7 @@ void incrementSeconds() {
       }
     }
   }
-  showColonDelimiter = !showColonDelimiter;
+  blinkSeconds = !blinkSeconds;
   nextSecond = millis()+1000;
 }
 
@@ -36,8 +36,8 @@ void loop() {
     alpha4.writeDigitAscii(0, ' ');
   else
     alpha4.writeDigitAscii(0, '0'+(hours/10), hours>=12);
-  alpha4.writeDigitAscii(1, '0'+(hours%10), showColonDelimiter);
+  alpha4.writeDigitAscii(1, '0'+(hours%10), true);
   alpha4.writeDigitAscii(2, '0'+(minutes/10));
-  alpha4.writeDigitAscii(3, '0'+(minutes%10));
+  alpha4.writeDigitAscii(3, '0'+(minutes%10), blinkSeconds);
   alpha4.writeDisplay();
 }
