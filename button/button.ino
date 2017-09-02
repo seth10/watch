4f70d0e553802a5321edefba65078cbfd75a3e42
Seth T <seth10@users.noreply.github.com>
2017-09-01 21:44:29 -0400

    Recognize first button press in sequence and simplify logic

--- button/button.ino
+++ button/button.ino
@@ -7,20 +7,19 @@ volatile byte sequence = 0;
 void isr() {
   delta = millis() - lastISR;
   lastISR = millis();
-  if (delta < 100) return;
-  if (delta < 1000) {
-    switch (sequence) {
-      case 1: minutes = (minutes+1)  % 60; break;
-      case 2: minutes = (minutes+10) % 60; break;
-      case 3: hours   = (hours+1)    % 24; break;
-      case 4: hours   = (hours+6)    % 24; break;
-    }
-  } else {
-    if      (delta > 7000) sequence = 0;
-    else if (delta > 5000) sequence = 4;
-    else if (delta > 3000) sequence = 3;
-    else if (delta > 2000) sequence = 2;
-    else                sequence = 1;
+  if (delta < 100) return; // debounce
+
+  /**/ if (delta > 5000) sequence = 0;
+  else if (delta > 4000) sequence = 4;
+  else if (delta > 3000) sequence = 3;
+  else if (delta > 2000) sequence = 2;
+  else if (delta > 1000) sequence = 1;
+
+  switch (sequence) {
+    case 1: minutes = (minutes+1)  % 60; break;
+    case 2: minutes = (minutes+10) % 60; break;
+    case 3: hours   = (hours+1)    % 24; break;
+    case 4: hours   = (hours+6)    % 24; break;
   }
 }
 
@@ -32,10 +31,10 @@ void setup() {
 
 void loop() {
   Serial.print('\r');
+  if (hours < 10) Serial.print('0');
   Serial.print(hours);
   Serial.print(':');
   if (minutes < 10) Serial.print('0');
   Serial.print(minutes);
-  if (hours < 10) Serial.print(' ');
   delay(50);
 }
