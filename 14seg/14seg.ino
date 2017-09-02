d52b34b6590fc8f095fdf1e6c3db7aa21e170ca8
Seth T <seth10@users.noreply.github.com>
2017-09-02 12:45:27 -0400

    Add blinking dot for seconds

--- 14seg/14seg.ino
+++ 14seg/14seg.ino
@@ -1,10 +1,10 @@
 #include "Adafruit_LEDBackpack.h"
 Adafruit_AlphaNum4 alpha4;
 
-volatile byte seconds = 0,
-              minutes = 30,
-              hours = 9;
-
+byte seconds = 0,
+     minutes = 30,
+     hours = 9;
+boolean showColonDelimiter = true;
 unsigned long nextSecond = millis()+1000;
 
 void incrementSeconds() {
@@ -20,6 +20,7 @@ void incrementSeconds() {
       }
     }
   }
+  showColonDelimiter = !showColonDelimiter;
   nextSecond = millis()+1000;
 }
 
@@ -35,7 +36,7 @@ void loop() {
     alpha4.writeDigitAscii(0, ' ');
   else
     alpha4.writeDigitAscii(0, '0'+(hours/10), hours>=12);
-  alpha4.writeDigitAscii(1, '0'+(hours%10), true);
+  alpha4.writeDigitAscii(1, '0'+(hours%10), showColonDelimiter);
   alpha4.writeDigitAscii(2, '0'+(minutes/10));
   alpha4.writeDigitAscii(3, '0'+(minutes%10));
   alpha4.writeDisplay();
